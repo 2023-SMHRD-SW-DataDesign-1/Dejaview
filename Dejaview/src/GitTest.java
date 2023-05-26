47ee957d42f71c5e73b2331c2d9c99a7933ead55
smhrd <smhrd@DESKTOP-N703G55>
2023-05-26 11:42:34 +0900

    230526 김지홍

--- Dejaview/src/GitTest.java
+++ Dejaview/src/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		
 		System.out.println("김지홍");		
-		System.out.println("한남욱");
+		System.out.println("한남욱");		
 		
 		System.out.println("이승훈");
 
