f7e393a69c7a8d7677ccc73fd4476705a39e9698
smhrd <smhrd@DESKTOP-JMD4RAM>
2023-05-30 15:07:47 +0900

    230530 0307 종합본

--- Dejaview/src/hong/test.java
+++ Dejaview/src/hong/test.java
@@ -156,17 +156,18 @@ public class test {
 
 		while (true) {
 
-			if (day == 0)
-				break;
+			System.out.println("어서오십시오.");
+			System.out.println("이곳은 로비입니다.");
 			System.out.println("남은 기한 " + day + "일");
-			System.out.println("로비입니다.");
-			System.out.println("1. 도박장 2. 사냥터 3. 골든벨");
+			System.out.println("오늘도 행운이 가득하시길.");
+			System.out.println("향할 곳을 정해주세요.");
+			System.out.println("[1] 도박장 [2] 사냥터 [3] 골든벨");
 			System.out.print("갈곳을 선택 하세요 >> ");
 			int s = sc.nextInt();
 			System.out.println();
 
 			if (s == 1) {
-
+				// 도박장 코드
 				System.out.println("====== 도박장 =======");
 				System.out.println("여기는 도박장... 일확천금을 노리시나요? 하이리스크 하이리턴... 거액을 걸어보세요");
 				System.out.println("게임을 시작합니다. 게임은 야바위게임~ 여러 컵 중에 공이 들어가있는 컵은 단 1개!!");
@@ -178,9 +179,9 @@ public class test {
 					int cupnum = sc.nextInt(); // 컵의 개수
 					int ball; // 컵에 들어있는 볼 위치
 					int guess;// 사용자가 고를 컵
-
+					int gold2;
 					System.out.print("골드를 얼마나 거시겠습니까? : ");
-					gold = sc.nextInt();
+					gold2 = sc.nextInt();
 
 					// 이어 붙일때 전체 골드에서 차감할 기능 추가 + 걸 돈의 최대값 지정
 
@@ -192,12 +193,13 @@ public class test {
 						guess = sc.nextInt();
 						if (ball == guess) {
 							System.out.println("축하드립니다");
-							gold *= 1.5;
-							System.out.println("획득한 골드는 " + gold + "G 입니다");
+							gold2 *= 1.5;
+							System.out.println("획득한 골드는 " + gold2 + "G 입니다");
+							gold += gold2;
 
 						} else {
 							System.out.println("틀렸습니다~~~~");
-							gold = 0;
+							gold2 = 0;
 						}
 
 					} else if (cupnum == 3) {
@@ -207,12 +209,15 @@ public class test {
 						guess = sc.nextInt();
 						if (ball == guess) {
 							System.out.println("축하드립니다");
-							gold *= 2;
-							System.out.println("획득한 골드는 " + gold + "G 입니다");
+							gold2 *= 2;
+							System.out.println("획득한 골드는 " + gold2 + "G 입니다");
+							gold += gold2;
+							System.out.println("현재 보유 골드는 : " + gold);
 
 						} else {
 							System.out.println("틀렸습니다~~~~");
-							gold = 0;
+							gold2 = 0;
+							System.out.println("현재 보유 골드는 : " + gold);
 						}
 
 					} else if (cupnum == 5) {
@@ -222,12 +227,15 @@ public class test {
 						guess = sc.nextInt();
 						if (ball == guess) {
 							System.out.println("축하드립니다");
-							gold *= 4;
-							System.out.println("획득한 골드는 " + gold + "G 입니다");
+							gold2 *= 4;
+							System.out.println("획득한 골드는 " + gold2 + "G 입니다");
+							gold += gold2;
+							System.out.println("현재 보유 골드는 : " + gold);
 
 						} else {
 							System.out.println("틀렸습니다~~~~");
-							gold = 0;
+							gold2 = 0;
+							System.out.println("현재 보유 골드는 : " + gold);
 						}
 					} else {
 						System.out.println("컵의 개수는 2개, 3개, 5개 중 고르세요");
@@ -240,7 +248,7 @@ public class test {
 
 			if (s == 2) {
 
-				System.out.println("야생의 고블린이 나타났다.");
+				System.out.println("============야생의 고블린이 나타났다.============");
 
 				System.out.println("⠀⠀⠀⠀⠀⠀⣀⢀⠀");
 				System.out.println("⠀⠀⠀⠀⠀⠀⠺⠾⡀⣀");
@@ -303,10 +311,60 @@ public class test {
 
 			if (s == 3) {
 				// 골든벨 코드
+				System.out.println("===========================골든벨===============================");
+				System.out.println("골든벨은 easy 문제와 hard 문제로 구성되어 있으며 연산문제의 정답을 맞추는 미니게임입니다.");
+				System.out.println("easy문제는 덧셈문제, hard 문제는 곱셈 문제입니다. ");
+				System.out.println();
+
+				// easy 문제(덧셈)-1문제
+
+				System.out.println("[easy 문제]");
+				int num1 = rand.nextInt(1, 15);
+				int num2 = rand.nextInt(1, 15);
+
+				System.out.print(num1 + "+" + num2 + "=");
+				int answer = sc.nextInt();
+
+				if (num1 + num2 == answer) {
+
+					gold += 30;
+					System.out.println("정답입니다! 30점을 획득하였습니다.");
+					System.out.println("현재 골드: " + gold);
+
+				} else if (num1 + num2 != answer) {
+
+					System.out.println("오답입니다. 다시 한 번 도전해보세요.");
+				}
+
+				// hard 문제(곱하기)-1문제
+				System.out.println();
+				System.out.println("[hard 문제]");
+				System.out.print(num1 + "*" + num2 + "=");
+				answer = sc.nextInt();
+
+				if (num1 * num2 == answer) {
+
+					gold += 50;
+					System.out.println("정답입니다! 50점을 획득하였습니다.");
+					System.out.println("현재 골드: " + gold);
+
+				} else if (num1 * num2 != answer) {
+
+					System.out.println("오답입니다. 다시 한 번 도전해보세요.");
+				}
+
+				System.out.println();
+				System.out.println("퀴즈를 풀 수 있는 기회가 소진되었습니다. 내일 다시 도전해주세요.");
 			}
 
 			day--;
+
+			if (day == 0) {
+				System.out.println("어서오십시오.");
+				System.out.println("빚을 갚을 날이 도래했습니다.");
+				System.out.println("빚 400G를 차감한 현재 골드는 : " + (gold - 400));
+				break;
+			}
 		}
 	}
-
 }
